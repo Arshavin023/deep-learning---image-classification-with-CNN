6d8144235136e3004c0606777903be5c8760cfd6
Arshavin023 <uchejudennodim@gmail.com>
2023-12-29 15:52:48 +0100

    data ingestion modular encoding completed

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from src.cnnClassifier.utils import logger
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info('Welcome to our custom logs')
+STAGE_NAME_01 = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>> {STAGE_NAME_01} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> {STAGE_NAME_01} completed <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
