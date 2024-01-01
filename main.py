f9a8008b4862562bc745bf7d59dc9aec9443435c
Arshavin023 <uchejudennodim@gmail.com>
2024-01-01 11:07:19 +0100

    dvc implemented

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/Arshavin023/deep-learning
 os.environ["MLFLOW_TRACKING_USERNAME"]="Arshavin023"
 os.environ["MLFLOW_TRACKING_PASSWORD"]="3a6add494bc8a69b0623dd2f8030d353dde15622"
 
-# STAGE_NAME_01 = "Data Ingestion stage"
-# try:
-#     logger.info(f">>>> {STAGE_NAME_01} started <<<<<")
-#     data_ingestion = DataIngestionTrainingPipeline()
-#     data_ingestion.main()
-#     logger.info(f">>>>> {STAGE_NAME_01} completed <<<<<<\n\nx========x")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+STAGE_NAME_01 = "Data Ingestion stage"
+try:
+    logger.info(f">>>> {STAGE_NAME_01} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> {STAGE_NAME_01} completed <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 STAGE_NAME_02 = "Prepare Base Model"
 try:
